94aaab1c98494b339e146d8cce6c0ae1688ce35c
Fabrício Meira <fabriciomeira03@gmail.com>
2023-04-08 20:00:50 -0300

    atualização

---
+++ 01-Fundamentals-Part-1/starter/myscript.js
@@ -0,0 +1,29 @@
+
+      let js = 'amazing';
+      if (js === 'amazing') alert ('Javascript is FUN!');
+
+console.log (40+8+23-10);
+
+let firstName = 'Jonas';
+/* this creates a fix variable */
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+/*this is usefull because if i want to change the variable, i can do it just in one place
+--------------
+this a the great advantage of variables
+-------------
+*/ 
+
+let firstNamePerson /*have in mind that this writting form is standard in javascript */
+
+let myFirstJob = 'Prorammer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher'
+
+/*the first two variables are the correct form */
+
+console.log(myFirstJob);
