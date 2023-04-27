5dd92b801b244c6082306da6e49480143d5918fb
Fabrício Meira <fabriciomeira03@gmail.com>
2023-04-27 10:12:30 -0300

    atualizacao

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,13 +4,53 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); 
-console.log(btnsOpenModal);
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden'); 
+    overlay.classList.remove('hidden'); 
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 
 for(let i = 0; i < btnsOpenModal.length; i++) 
-//this line work through all three buttons. we need to add an event list (or event handler, they are the same).
+btnsOpenModal[i].addEventListener('click', openModal);    
 
-//no braces {} to define the block. just like if...else statemen, i actually don't have to create the block to execute over & over again.
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-console.log(btnsOpenModal[i].textContent);
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+
+if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    //means: if modal does not contain hidden class, then close the modal
+    //IF the press key is 'escape' AND IF modal does not contain the 'hidden class' THEN execute this function to close the modal
+    closeModal();
+    }
+});
+  
 
 
+/*
+////// FIRST CODE. More intuitive, not better though.
+
+btnsOpenModal[i].addEventListener('click', function() {
+    console.log('Button clicked');
+    modal.classList.remove('hidden'); 
+    //no dot ~ '.hidden'
+    overlay.classList.remove('hidden'); 
+    //overlay is click outside to close
+});
+
+btnCloseModal.addEventListener('click', function() {
+modal.classList.add('hidden');
+overlay.classList.add('hidden');
+})
+
+*/
+
+//CSS. display: none means not visible. if we wanted viile would be display: block
