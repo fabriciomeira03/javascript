75c710a9e4964c022522dc9252c30d0b6591db50
Fabrício Meira <fabriciomeira03@gmail.com>
2023-04-28 11:37:52 -0300

    atualização

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements 
+const Player0El = document.querySelector('.player--0');
+const Player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score0El.textcontent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -28,12 +32,23 @@ btnRoll.addEventListener('click', function () {
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
-    //3. Check for rolled 1: if true, switch to next player
-    if(dice !== 1) {
-    //add dice to current score
-    currentScore += dice;
-    current0El.textContent = currentScore; //CHANGE LATER
+    //3. Check for rolled 1
+    if (dice !== 1) {
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  
+
+     //add dice to current score
+    //when the dice is not at 1 it's added to the current score.
+    //whenever player rolls 1 we lose all the score & enter the 'else' block switching to next player round.
+
     } else {
     //switch to next player
-    }
+    document.getElementById (`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //reasign the activeplayer.
+    currentScore = 0;  
+    Player0El.classList.toggle('player--active'); 
+    Player1El.classList.toggle('player--active'); 
+}
 });
