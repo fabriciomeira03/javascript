c5717823e6199ff76710ffeebdf5c69e62f9f3c1
Fabrício Meira <fabriciomeira03@gmail.com>
2023-05-01 00:04:46 -0300

    atualização

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+  /*
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,6 +13,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainMenu]];
+  }
 };
 
 const arr = [2, 3, 4];
@@ -23,12 +27,44 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(x, y, z);
 
-const [first, ,  second] = restaurant.categories; //'restaurant.categories' is the array which we want to abstract.
-console.log(first, second);
+let [main, ,  secondary] = restaurant.categories; //'restaurant.categories' is the array which we want to abstract.
+console.log(main, secondary);
 
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse); 
 
+// NESTED DESTRUCTURING
+const nested = [2, 4 [5, 6]]; 
+//const [i, ,] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
 
 /*
+//switch this two variables without desturcturing.
+const temp = main;
+main = secondary;
+secondary = temp; 
+console.log(main, secondary);
+
+
+
+//same example with destructuring
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+*/
+
+////////////////////////////////////////////////////////
+//Destructuring Objects
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
   openingHours: {
     thu: {
       open: 12,
@@ -43,5 +79,48 @@ console.log(first, second);
       close: 24,
     },
   },
+    
+    order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainMenu]];
+    },
+
+    orderDelivery: function (obj) {
+      console.log(obj);
+    },
+  
+    orderDelivery: function ({starterIndex, mainIndex, time, address}) {
+      console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}}`);
+    }
 };
-*/
+
+restaurant.orderDelivery ({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const {name, openingHours, categories} = restaurant;
+//3 variables based in the restaurant object.
+console.log(name, openingHours, categories);
+
+//Default variables (important set them when get data from 3rd party and doesn't know how the code is)
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu,starters);
+
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({a, b} = obj);
+
+//Nested objects
+const {fri: {open: o, close: c}} = openingHours;
+console.log(fri);
+console.log(o, c);
+
+
