b9e2abae355f9f9f1e507a69816146f69e9aac14
Fabrício Meira <fabriciomeira03@gmail.com>
2023-05-05 17:56:16 -0300

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,7 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-  /*
+/*
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -28,18 +28,20 @@ const [x, y, z] = arr;
 console.log(x, y, z);
 
 let [main, ,  secondary] = restaurant.categories; //'restaurant.categories' is the array which we want to abstract.
+
 console.log(main, secondary);
 
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse); 
 
+
 // NESTED DESTRUCTURING
 const nested = [2, 4 [5, 6]]; 
-//const [i, ,] = nested;
+//const [i, ,j] = nested;  (old way)
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
-/*
+
 //switch this two variables without desturcturing.
 const temp = main;
 main = secondary;
@@ -47,16 +49,25 @@ secondary = temp;
 console.log(main, secondary);
 
 
-
 //same example with destructuring
 [main, secondary] = [secondary, main]
 console.log(main, secondary);
+
+//Default values
+const [p, q, r] = [8, 9]; //here positions only in [0] and [1]
+console.log(p, q, r);
+
+//Default values
+const [p=1, q=1, r=1] = [8, 9];
+console.log(p, q, r);
+
+//default values are usefull for when we've got data from API  
 
 */
 
 ////////////// Destructuring Objects ///////////////////////
 
-/*
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -109,7 +120,12 @@ const {name, openingHours, categories} = restaurant;
 console.log(name, openingHours, categories);
 
 //Default variables (important set them when get data from 3rd party and doesn't know how the code is)
-const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+
+const {
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags
+} = restaurant;
 console.log(restaurantName, hours, tags);
 
 const {menu = [], starterMenu: starters = []} = restaurant;
@@ -119,8 +135,8 @@ console.log(menu,starters);
 //Mutating variables
 let a = 111;
 let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-({a, b} = obj);
+const obj = { a: 23, b: 7, c: 14 }; 
+({a, b} = obj); //destructuring here
 
 //Nested objects
 const {fri: {open: o, close: c}} = openingHours;
