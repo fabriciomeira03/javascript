678ab5b72746007d307cf0c2f10a36029e416cda
Fabricio Meira <fabriciomeira03@gmail.com>
2023-05-25 00:32:37 -0300

    update

--- 11-Arrays-Bankist/starter/arrayspractice.js
+++ 11-Arrays-Bankist/starter/arrayspractice.js
@@ -12,6 +12,19 @@ const bankDepositSum = accounts
 
 console.log(bankDepositSum);
 
-
+/*
 //Exercise02: how many accounts with at least 1000 usd
+const numDeposits1000 = accounts
+    .flatMap(acc => acc.movements) //counting deposits
+    .filter(mov => mov >= 1000).length;
 
+console.log(numDeposits1000);
+*/
+
+
+// ----- below, the same thing using reduce
+const numDeposits1000 = accounts
+    .flatMap(acc => acc.movements)
+    .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+
+console.log(numDeposits1000);
