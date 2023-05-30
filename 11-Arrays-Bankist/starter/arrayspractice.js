26ca1db54ccfb13ab14aa9364bd386643e95ce0d
Fabricio Meira <fabriciomeira03@gmail.com>
2023-05-30 00:47:02 -0300

    update

--- 11-Arrays-Bankist/starter/arrayspractice.js
+++ 11-Arrays-Bankist/starter/arrayspractice.js
@@ -56,3 +56,22 @@ const {deposits, withdrawls} = accounts
 console.log(deposits, withdrawls);
 
 
+
+//4. this is a nice tittle -> This Is a Nice Title
+
+const convertTitleCase = function(title) {
+    const expections = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+    const titleCase = title
+        .toLowerCase()
+        .split(' ')
+        .map(word => word[0].toUpperCase() + word.slice(1));
+        
+    return titleCase;
+
+}
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+
+
