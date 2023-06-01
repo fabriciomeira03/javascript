4c6367b9a90e924ef1b1d7c4fecab704db6304e4
Fabricio Meira <fabriciomeira03@gmail.com>
2023-06-01 00:15:39 -0300

    update

--- 09-Data-Structures-Operators/final/codingchallenge.js
+++ 09-Data-Structures-Operators/final/codingchallenge.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const game = {
+const games = {
     team1: 'Bayern Munich',
     team2: 'Borrussia Dortmund',
     players: [
@@ -19,7 +19,44 @@ const game = {
         ],
         
         [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
 
-        ]
-    ]
-}
+        ],
+    ],
+
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+date: 'Nov 0th, 2037',
+odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+    },
+};
+
+
+//1. 
+const [players1, players2] = games.players //p1 & p2 are the result of destructuring 'game.players'
+console.log(players1, players2);
+
+
+//2. 
+//whenever we hear "remaining" we need to remember the 'rest' syntax which is [...name];
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4. 
