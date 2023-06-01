3d3daeb8ec0cd17e7ec39c3a2627b625dab5acb4
Fabricio Meira <fabriciomeira03@gmail.com>
2023-05-31 23:56:36 -0300

    update

--- 09-Data-Structures-Operators/final/codingchallenge.js
+++ 09-Data-Structures-Operators/final/codingchallenge.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        
+        [
+
+        ]
+    ]
+}
