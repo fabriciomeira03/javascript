3ef36c0dde693bc18b34852f81816d95bab83a6f
Fabrício Meira <fabriciomeira03@gmail.com>
2023-06-05 00:33:20 -0300

    update

--- 09-Data-Structures-Operators/final/codingchallenge.js
+++ 09-Data-Structures-Operators/final/codingchallenge.js
@@ -46,8 +46,9 @@ odds: {
 
 
 //1. 
-const [players1, players2] = games.players //p1 & p2 are the result of destructuring 'game.players'
+const [players1, players2] = games.players //p1 & p2 are the result of destructuring 'game.players'. result is TWO array's (side by side) with all elements
 console.log(players1, players2);
+
 
 
 //2. 
@@ -55,7 +56,8 @@ console.log(players1, players2);
 const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 
-//3.
+
+//3. with this syntax we get ONE array with all 22 elements (p01 & p02)
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
