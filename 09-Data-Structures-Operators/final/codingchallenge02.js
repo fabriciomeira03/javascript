6ecacf79f3084902c79e0e35a27659b0e1501b1e
Fabrício Meira <fabriciomeira03@gmail.com>
2023-06-13 23:22:02 -0300

    update

--- 09-Data-Structures-Operators/final/codingchallenge02.js
+++ 09-Data-Structures-Operators/final/codingchallenge02.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const games = {
+const game = {
     team1: 'Bayern Munich',
     team2: 'Borrussia Dortmund',
     players: [
@@ -43,5 +43,31 @@ odds: {
     team2: 6.5,
     },
 };
+
+
+//1. destructured in "i" of index and then "player". the word 'entries' will give us the properties.
+for(const [i, player] of game.scored.entries())
+    console.log(`Goal ${i + 1}: ${player}`); //${i + 1} means it will start with goal number one.
+
+
+//2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+    average /= odds.length;
+console.log(average);
+
+//3. 
+for (const [team, odd] of Object.entries(game.odds))
+    {
+        console.log(team, odd);
+        const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+        console.log(`Odd of ${teamStr} ${odd}`);
+    }
+
+
+//note: in the array [] we use the .entries (game.scored.entries) to get the entries of the array.
+//but in the object we have to use 'Object.entries(game.odds)' AND THEN pass into the function the object we're interested in. 
+// ----- that's a fundamental difference we need be aware of ------
 
 
