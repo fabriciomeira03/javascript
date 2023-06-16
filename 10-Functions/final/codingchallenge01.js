00210e720ba61fff60dbfe200e61fbe09e878b5b
Fabrício Meira <fabriciomeira03@gmail.com>
2023-06-16 00:04:06 -0300

    update

---
+++ 10-Functions/final/codingchallenge01.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+    registerNewAsnwer() {
+
+//get answer
+        const answer = prompt(`${this.question}\n${this.options.join('\n')}) \n(Write Option Number)`);
+
+//register the answer
+typeof answer === 'number' && answer < this.answer.length && this.answers[answer]++;
+        },
+    };
+//poll.registerNewAnswer();
